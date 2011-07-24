3b48ce1368e02c34d8611c99c3535e9cda714e5b
Morteza Milani <mrtz.milani@googlemail.com>
2011-07-25 00:47:04 +0430

    Improved installation document.

---
+++ application/config/environment.example.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Environment configuration
+ * Copy this file to APPLICATION_PATH/config/environment.php to define 
+ * the working environment.
+ *
+ * Posibile values are:
+ * - development
+ * - staging
+ * - production
+ * 
+ * Use the predefined constants as they improve code readability (no magic strings)
+ * and help avoiding typos.
+ *
+ * Information on the current environment will be also placed in the registry. Use the 
+ * registry to check the application's state. 
+ *
+ * Example:
+ *
+ * if (APPLICATION_ENV == 'production') {
+ * // bad
+ * }
+ *
+ * if (APPLICATION_ENV == APP_STATE_PRODUCTION) {
+ * // better
+ * }
+ *
+ * if (Zend_Registry::get('IS_PRODUCTION')) {
+ * // best
+ * }
+ *
+ * Available Zend_Registry keys are IS_PRODUCTION, IS_STAGING, IS_DEVELOPMENT
+ *
+ * @package application_config
+ * @copyright Copyright (c) 2011, Morteza Milani
+ */
+define('APP_STATE_PRODUCTION', 'production');
+define('APP_STATE_STAGING', 'staging');
+define('APP_STATE_DEVELOPMENT', 'development');
+define('APPLICATION_ENV', APP_STATE_PRODUCTION);
--- docs/install.md
+++ docs/install.md
@@ -1,10 +1,16 @@
 Install
 =======
 
-* Edit app.ini file and place it under application/config/
-* create tables using kode.sql
+* Create tables using kode.sql
+    * create a database in mysql. Name it what ever you like.
+    * import `docs/sql/kode.sql` into your newly created database.
+* Edit `docs/app.ini` file and place it under application/config/
+    * set `resources.db.params.dbname` to the name you just created. default is `kode`
+    * set mysql username: `resources.db.params.username = "USERNAMEHERE"`
+    * set mysql password: `resources.db.params.password = "PASSWORDHERE"`
+    * config everything else there if you like
+* Rename environment.example.php to environment.php and configure it base on your needs.
 * create an apache alias or vhost and point it to kode/public/
-* create another apache alias or vhost and point it to kode/public/admin
 * create these folders under kode/data and set necessary permissions:
     cache/
     logs/admin/missing_translations
