abdd3ca18ed0cdf3b5b035d8f70ffcd9009d3ae0
Morteza Milani <mrtz.milani@googlemail.com>
2012-02-18 21:14:59 +0330

    Default environment changed to production

--- application/config/environment.php
+++ application/config/environment.php
@@ -37,4 +37,4 @@
 define('APP_STATE_PRODUCTION', 'production');
 define('APP_STATE_STAGING', 'staging');
 define('APP_STATE_DEVELOPMENT', 'development');
-define('APPLICATION_ENV', APP_STATE_DEVELOPMENT);
+define('APPLICATION_ENV', APP_STATE_PRODUCTION);
